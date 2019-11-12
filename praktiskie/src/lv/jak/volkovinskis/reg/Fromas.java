f757731ce3e78b553c7bed2a6b66ddf0a2dc4ff3
lietotajs <lietotajs@215-04>
2019-11-12 13:56:41 +0200

    dwasda

--- praktiskie/src/lv/jak/volkovinskis/reg/Fromas.java
+++ praktiskie/src/lv/jak/volkovinskis/reg/Fromas.java
@@ -3,6 +3,7 @@ package lv.jak.volkovinskis.reg;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -22,19 +23,20 @@ public class Fromas extends JFrame implements ActionListener {
 
 	///Definešana
 	JPanel panel;
-	JLabel vards,uzvards,elektriba,rekinaNR,emailL,telefonaNR;
-	JTextField vardsField, uzvardsField, rekinaNRF, emailF,telefonaNRT;
+	JLabel vards,uzvards,elektriba,rekinaNR,emailL,telefonaNR, summa;
+	JTextField vardsField, uzvardsField, rekinaNRF, emailF,telefonaNRT, elektribaF;
 	JList<String> availableList, selectedList;
-	JButton allToSelectedBTN, toSelectedBTN, allToAvailableBTN, toAvailableBTN, addBTN, removeBTN, clearBTN, okBTN,cancelBTN;
+	JButton allToSelectedBTN, toSelectedBTN, allToAvailableBTN, toAvailableBTN, addBTN, cancelBTN;
 
 	DefaultListModel<String> availableModel = new DefaultListModel<String>();
 	DefaultListModel<String> selectedModel = new DefaultListModel<String>();
-
+	
+	
 	///Formas Izveidošana, izsaukšana
 	public Fromas() {
-		setBounds(100, 150, 600, 500);
+		setBounds(100, 150, 640, 250);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("Elektrum.lv");
+		setTitle("TET.lv");
 		setResizable(false);
 		init();
 
@@ -50,7 +52,7 @@ public class Fromas extends JFrame implements ActionListener {
 		vards.setBounds(-120, 10, 230, 30);
 		
 		uzvards = new JLabel("Uzvārds:", SwingConstants.RIGHT);
-		uzvards.setBounds(110, 10, 230, 30);
+		uzvards.setBounds(150, 10, 230, 30);
 		
 		rekinaNR = new JLabel("Rēķina Numurs:", SwingConstants.RIGHT);
 		rekinaNR.setBounds(-120, 50, 230, 30);
@@ -59,34 +61,67 @@ public class Fromas extends JFrame implements ActionListener {
 		rekinaNRF.setBounds(112, 50, 170, 30);
 		
 		emailL = new JLabel("E-pasts:", SwingConstants.RIGHT);
-		emailL.setBounds(110, 50, 230, 30);
+		emailL.setBounds(150, 50, 230, 30);
 		///Field
 		emailF  = new JTextField();
-		emailF.setBounds(345, 50, 170, 30);
+		emailF.setBounds(385, 50, 170, 30);
 		///Field
 		uzvardsField = new JTextField();
-		uzvardsField.setBounds(345, 10, 170, 30);
+		uzvardsField.setBounds(385, 10, 170, 30);
 		///Field
 		vardsField = new JTextField();
 		vardsField.setBounds(112, 10, 170, 30);
 		
 		telefonaNR = new JLabel("Tālrunis:", SwingConstants.RIGHT);
 		telefonaNR.setBounds(-120, 90, 230, 30);
-		
+		///Field
 		telefonaNRT = new JTextField();
 		telefonaNRT.setBounds(112, 90, 170, 30);
 		
-		elektriba = new JLabel();
-		elektriba.setBounds(x, y, width, height);
+		elektriba = new JLabel("Patēriņš(kWh):", SwingConstants.RIGHT );
+		elektriba.setBounds(150, 90, 230, 30);
+		///Field
+		elektribaF = new JTextField();
+		elektribaF.setBounds(385, 90, 170, 30);
 		
+		addBTN = new JButton("Aprēķināt");
+		addBTN.setBounds(130, 165, 100, 30);
+		addBTN.addActionListener(this);
+		
+		
+		summa = new JLabel("Summa:",SwingConstants.RIGHT);
+		
+		
+		cancelBTN = new JButton("Cancel");
+		cancelBTN.setBounds(20, 165, 100, 30);
+		cancelBTN.addActionListener(this);
+		
+			
 		panel.add(vards);panel.add(uzvards);panel.add(uzvardsField);panel.add(vardsField);
 		panel.add(emailL);panel.add(rekinaNR);panel.add(rekinaNRF);panel.add(emailF);
-		panel.add(telefonaNR);panel.add(telefonaNRT);panel.add(elektriba);
-		
+		panel.add(telefonaNR);panel.add(telefonaNRT);panel.add(elektriba);panel.add(elektribaF);
+		panel.add(addBTN);panel.add(cancelBTN);panel.add(summa);
 		
 	}
 
 	///Metode
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == addBTN) {
+			elektribaF.getText();
+			int aprekinatspaterins = Integer.parseInt(elektribaF.getText());
+			summa.removeAll();
+			double ew = (double) (aprekinatspaterins * 0.05097);
+			DecimalFormat newrandom = new DecimalFormat("#.##");
+	        summa.setText("Summa: " + newrandom.format(ew) + ("€"));
+			summa.setBounds(-40, 140, 245, 30);
+			 
+			 
+			 if (e.getSource() == cancelBTN) {
+				 this.dispose();
+				System.exit(0); 	///program closed
+			}
+			 
+			 
+		}
 	}
 }
